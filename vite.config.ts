706ff1fea8e68a1cc868454262a617980c0dedfb
Cecilia Ortiz <116905281+cecilitaortiz@users.noreply.github.com>
2025-07-14 15:31:05 +0000

    Guía 17

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,83 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
+import { VitePWA } from 'vite-plugin-pwa'
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/dashboard",
+  base: "/dashboard/",
 
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      devOptions: {
+        enabled: true
+      },
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png'],
+      manifest: {
+        id: '/dashboard/',
+        name: 'Dashboard del Clima - Proyecto 04',
+        short_name: 'Dashboard del Clima',
+        description: 'Proyecto 04 - dashboard del clima desarrollado con React y MUI',
+        theme_color: '#D3D1D1',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+          {
+            src: "pwa-maskable-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+            purpose: "maskable"
+          },
+          {
+            src: "pwa-maskable-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "maskable"
+          },
+        ]
+      },
+      workbox: {
+        runtimeCaching: [
+          {
+            // Cache para recursos estáticos (JS, CSS, HTML)
+            urlPattern: /^https:\/\/.*\.(?:js|css|html|png|jpg|jpeg|svg|woff2?)$/,
+            handler: 'CacheFirst',
+            options: {
+              cacheName: 'static-resources',
+              expiration: {
+                maxEntries: 50,
+                maxAgeSeconds: 60 * 60 * 24 * 30, // 30 días
+              },
+            }
+          },
+          
+          {
+            // Tu configuración actual para la API
+            urlPattern: /^https:\/\/api\.open-meteo\.com\/.*$/,
+            handler: 'NetworkFirst',
+            options: {
+              cacheName: 'open-meteo-cache',
+              expiration: {
+                maxEntries: 10,
+                maxAgeSeconds: 60 * 60 * 24, // 1 día
+              },
+              cacheableResponse: {
+                statuses: [0, 200],
+              }
+            }
+          }
+        ]
+      }
+    })
+
+  ],
 
 })
